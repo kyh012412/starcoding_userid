b0b8e559097f99e2985317bfd3c0bb2e4f01f659
kyh012412 <kyh0124.12@gmail.com>
2024-03-07 21:44:42 +0900

    ex1goodcase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useId } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyInput />
+      <MyInput />
+    </div>
+  );
+}
+
+function MyInput() {
+  const id = useId();
+  console.log(id);
+  return (
+    <div>
+      <label htmlFor={`${id}-name`}>이름</label>
+      <input id={`${id}-name`} type="text" />
+
+      <label htmlFor={`${id}-age`}>나이</label>
+      <input id={`${id}-age`} type="text" />
     </div>
   );
 }
